f057cb2e1972c032eb90d60ae1cab13e112bfb00
get <249628024@qq.com>
2026-01-23 23:03:53 +0800

    init

---
+++ backend/src/main/java/com/smartcharger/SmartChargerApplication.java
@@ -0,0 +1,12 @@
+package com.smartcharger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChargerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartChargerApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/smartcharger/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.smartcharger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: sonicge
+ * @CreateTime: 2026-01-23
+ */
+
+@RestController
+@RequestMapping
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "健康";
+    }
+}
