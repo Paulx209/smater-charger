354dfc692960cdb06ae2d5d9b7c398b1ad6292e4
get <249628024@qq.com>
2026-01-24 10:25:42 +0800

    feat(smart-charger):新增业务异常类 & 全局异常处理器

---
+++ backend/src/main/java/com/smartcharger/common/exception/BusinessException.java
@@ -0,0 +1,41 @@
+package com.smartcharger.common.exception;
+
+import com.smartcharger.common.result.ResultCode;
+import lombok.Getter;
+
+/**
+ * 业务异常类
+ */
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    /**
+     * 错误信息
+     */
+    private final String message;
+
+    public BusinessException(ResultCode resultCode) {
+        super(resultCode.getMessage());
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+    }
+
+    public BusinessException(ResultCode resultCode, String message) {
+        super(message);
+        this.code = resultCode.getCode();
+        this.message = message;
+    }
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+}
